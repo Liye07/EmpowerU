895b642e5005fe0670a2b9003fe8f9797db6630b
TLuyanda <92914946+TLuyanda@users.noreply.github.com>
2024-10-11 15:29:56 +0200

    Report Model

---
+++ Models/Report.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpowerU.Models
+{
+    public class Report
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReportID { get; set; }
+
+        [Required]
+        public int BusinessID { get; set; }
+
+        [Required]
+        [StringLength(90)]
+        public string DateRange { get; set; }
+
+        [Required]
+        public string Metrics { get; set; }
+
+        [ForeignKey(nameof(BusinessID))]
+        public Business Business { get; set; }
+    }
+}
