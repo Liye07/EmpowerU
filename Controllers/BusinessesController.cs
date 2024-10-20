eed8a3e6a4e9bd681dd31b06840a1282b46fc5b3
Liyema Booi <s225025426@mandela.ac.za>
2024-10-20 12:44:31 +0200

    Business & Consumer dashboards(basic design)

--- Controllers/BusinessesController.cs
+++ Controllers/BusinessesController.cs
@@ -34,6 +34,11 @@ namespace EmpowerU.Controllers
             return View(businesses);
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         // GET: Businesses/Details/5
         public async Task<IActionResult> Details(int? id)
         {
--- Controllers/ConsumersController.cs
+++ Controllers/ConsumersController.cs
@@ -32,6 +32,11 @@ namespace EmpowerU.Controllers
             return View(await _context.Consumers.ToListAsync());
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         // GET: Consumers/Details/5
         public async Task<IActionResult> Details(int? id)
         {
